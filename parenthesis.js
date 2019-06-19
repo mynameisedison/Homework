02a450dcee2a44e2627dddfbff998db4ec5f3180
Edison <edisontoole@gmail.com>
2019-06-19 11:44:11 -0400

    ok now parenths it works

--- parenthesis.js
+++ parenthesis.js
@@ -2,19 +2,27 @@ let parenthesis = (string) =>{
 
   let stack = [] //initializing stack for parenths
   let result = ""
-  if(!string.length) return "error no input"
+
+  if(string === "" || string === " ") return "error no input"
   //for loop to search through input string
   for (let i = 0; i < string.length; i++) {
-    string[i]==="("?stack.push(string[i]):"" //ternary deciding to push onto stack if left parenth is detected
-    string[i]===")"?stack.pop():"" //ternary popping off last stacked element if right parenth is detected
+    let last = ""
+    //if statement deciding to push onto stack if left parenth is detected
+    if (string[i]==="(") stack.push(string[i])
+     //if right parenth is detected pop last element off stack and check if it matches
+    if (string[i]===")") {
+      last = stack.pop()
+      if (last !== "(") return false
   }
+  }
+
 
   //ternary deciding if stack length is 0 to return true because they all popped off, and false if anything is left inside
   stack.length === 0 ? result = true : result = false
   return result
 }
 
-console.log(parenthesis("((()))"));
+console.log(parenthesis("(((()((((000)))))))"));
 
 // let string = ")))((("
 // let string = "((())"
