38cc5f0102675315b668306a4b07e0b2309340e4
Edison <edisontoole@gmail.com>
2019-09-21 17:24:27 -0400

    restarted filesorter

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Availity-Homework
+# Homework
 
 Full Stack Technical Assignment for Availity
 
 Questions 1-3 in Short Answer Qs word doc
 4 is in parenthesis file
 5 is in filesorter file
-6 is in registration file 
+6 is in registration file
--- filesorter/filesorter2.js
+++ filesorter/filesorter2.js
@@ -1,15 +1,50 @@
-const csv=require("csvtojson");
+const csv = require("csvtojson");
+const { Parser } = require('json2csv');
+var file = require('file-system');
+var fs = require('fs');
 const csvFilePath='sampledata.csv'
+const dir = './companies'
 
-let convert = async()=>{
-  const jsonArray=await csv().fromFile(csvFilePath);
-  console.log(jsonArray);
+let names = []
+let companies = []
+
+if (!fs.existsSync(dir)){
+  fs.mkdirSync(dir)
 }
 
-// csv()
-// .fromFile(csvFilePath)
-// .then((jsonObj)=>{
-//     console.log(jsonObj);
-// })
+let convert = async() => {
+  const jsonArray=await csv().fromFile(csvFilePath)
+  jsonArray.forEach(e=>createCompanyFile(e))
+  jsonArray.sort(compare)
+  // console.log(jsonArray);
+}
+
+let compare = (nameOne, nameTwo) => {
+  var splitOne = nameOne["Name"].split(" ")
+  var splitTwo = nameTwo["Name"].split(" ")
+  var lastOne = splitOne[splitOne.length - 1]
+  var lastTwo = splitTwo[splitTwo.length - 1]
+
+  if (lastOne < lastTwo) return -1
+  if (lastOne > lastTwo) return 1
+  return 0
+}
+
+let createCompanyFile = (obj) => {
+  let filepath=dir + "/" + obj["Insurance_Company"]
+  if(!companies.includes(obj["Insurance_Company"])){
+    companies.push(obj["Insurance_Company"])
+    fs.appendFileSync(filepath,obj["Name"],(err)=>{
+      if(err) throw err
+      console.log("file has been saved");
+    })
+    // console.log(companies);
+  }
+  // else fs.appendFile(filepath,obj["Name"],(err) => {
+  // if (err) throw err;
+  // console.log('appended to file!');
+// });
+}
+
 
 convert()
