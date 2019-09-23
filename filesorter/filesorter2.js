f902fd7565060f5f648a10b909f3c3dd870372cd
Edison <edisontoole@gmail.com>
2019-09-23 17:34:31 -0400

    working on filesorter v2 and re added v1

--- filesorter/filesorter2.js
+++ filesorter/filesorter2.js
@@ -1,8 +1,19 @@
-const csv = require("csvtojson");
+//  read the content of the file and separate enrollees by insurance company in its own file.
+//  sort the contents of each file by last and first name (ascending). 
+//  Lastly, if there are duplicate User Ids for the same Insurance Company, then only the record with
+//  the highest version should be included. The following data points are included in the file:
+// •	User Id (string)
+// •	First and Last Name (string)
+// •	Version (integer)
+// •	Insurance Company (string)
+
+
+const csvtojson = require("csvtojson");
 const { Parser } = require('json2csv');
 var file = require('file-system');
 var fs = require('fs');
 const csvFilePath='sampledata.csv'
+
 const dir = './companies'
 
 let names = []
@@ -13,10 +24,10 @@ if (!fs.existsSync(dir)){
 }
 
 let convert = async() => {
-  const jsonArray=await csv().fromFile(csvFilePath)
-  jsonArray.forEach(e=>createCompanyFile(e))
+  const jsonArray = await csvtojson().fromFile(csvFilePath)
   jsonArray.sort(compare)
-  // console.log(jsonArray);
+  createCompanyFile(jsonArray)
+  // console.log(jsonArray)
 }
 
 let compare = (nameOne, nameTwo) => {
@@ -31,19 +42,30 @@ let compare = (nameOne, nameTwo) => {
 }
 
 let createCompanyFile = (obj) => {
-  let filepath=dir + "/" + obj["Insurance_Company"]
-  if(!companies.includes(obj["Insurance_Company"])){
-    companies.push(obj["Insurance_Company"])
-    fs.appendFileSync(filepath,obj["Name"],(err)=>{
-      if(err) throw err
-      console.log("file has been saved");
-    })
-    // console.log(companies);
+  const fields = ["Userid","Name","Version","Insurance_Company"]
+  const json2csvParser = new Parser({ fields })
+  const csv = json2csvParser.parse(obj)
+  for (let i = 0; i < obj.length; i++) {
+    // console.log(obj[i])
+    let filePath=dir + "/" + obj[i]["Insurance_Company"]
+    try {
+      if (!fs.existsSync(filePath)) {
+        console.log("we are exist");
+        companies.push(obj["Insurance_Company"])
+        fs.appendFileSync(filepath,obj["Name"],(err)=>{
+          if(err) throw err
+          console.log("file has been saved")
+        })
+      }
+    } catch(err) {
+      console.error(err)
+    }
+
   }
-  // else fs.appendFile(filepath,obj["Name"],(err) => {
-  // if (err) throw err;
-  // console.log('appended to file!');
-// });
+}
+
+let deleteDuplicates = () => {
+
 }
 
 
